f9975e869af1e141669148b287f4e635ff438bd8
=weijiang1994 <ubuntu@localhost.localdomain>
2020-11-02 15:16:06 +0800

    test push

---
+++ test.py
@@ -0,0 +1,19 @@
+import time
+
+def run_time(func):
+	def wrap(*args, **kwargs):
+		st = time.time()
+		result = func(*args, **kwargs)
+		print('{} function running spend time is: {}'.format(func.__name__, time.time()-st))
+		return result
+	return wrap
+
+
+@run_time
+def test():
+	time.sleep(3)
+
+
+
+if __name__ == '__main__':
+	test()
